3f520d13d5a0804e3330c4254adcffde6ef940ba
20151064010180 <20151064010180@JC377047.ifrn.local>
2019-08-14 10:43:20 -0300

    Primeiro Commit

---
+++ src/erxerpds/Class3.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package erxerpds;
+
+/**
+ *
+ * @author Daniele Silva e Everson Fernandes
+ */
+public class Class3 {
+    public static void main(String[] args) {
+       
+        System.out.println("eai");
+        System.out.println("eai mano");
+        //metodo de retorno do nome eai 
+    }
+    public String getDani(){
+        return null;
+        
+        //Metodo para atribuir um valor ao nome Dani
+        //retorna um valor nulo 
+    }
+    public void setDani(){
+        System.out.println("Parabéns Dani, você venceu mais um trabalho de Nick sem saber de nada!! ;D");
+        //Faz pega algo 
+    }
+    }
+
